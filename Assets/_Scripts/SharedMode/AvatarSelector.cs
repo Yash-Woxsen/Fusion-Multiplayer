88420910811c8e376f2efd7af3dbf7b384b0517e
Yash Office PC <167862397+Yash-Woxsen@users.noreply.github.com>
2024-11-22 15:40:08 +0530

    Added UI For Lobby and Session

---
+++ Assets/_Scripts/SharedMode/AvatarSelector.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+public class AvatarSelector : MonoBehaviour
+{
+    public int SelectedAvatar = 0;
+    public void SetAvatarSelector(int avatar)
+    {
+        SelectedAvatar = avatar;
+    }
+}
